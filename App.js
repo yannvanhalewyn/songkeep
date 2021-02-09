99361d4ed4d175e322ea87e8184c6f585a4704f6
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2021-02-09 14:44:34 +0100

    Mess around with native components

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.button}>
+        <Button title="Foobar" onPress={() => {
+          console.log({count: count});
+          setCount(count+1);
+        }}>
+        </Button>
+      </View>
+      <View style={styles.button}>
+        <Button title="Reset" color="red" onPress={() => setCount(0)}></Button>
+      </View>
+      <Text style={{marginTop: 8}}>Count: {count}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    marginTop: 8,
+  }
 });
